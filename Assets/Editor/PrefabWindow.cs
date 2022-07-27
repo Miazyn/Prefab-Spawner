92d34447334ef380e78493a15659fc1a9f450f97
Miazyn <94353989+Miazyn@users.noreply.github.com>
2022-07-27 17:49:12 +0200

    Window now can undo and redo one action oooooooh

--- Assets/Editor/PrefabWindow.cs
+++ Assets/Editor/PrefabWindow.cs
@@ -9,8 +9,12 @@ public class PrefabWindow : EditorWindow
     GameObject myPrefab;
     static Vector3 objPos;
     static Vector3 objScale;
-    
-    
+
+    Vector3 lastPlacedPos;
+    Vector3 lastPlacedScale;
+    //Vector3 lastRotation;
+    GameObject contentsRoot;
+    GameObject lastPlacedObject;
     
     [MenuItem("Personal Tools/Prefab Editor")]
     public static void ShowWindow()
@@ -43,11 +47,28 @@ public class PrefabWindow : EditorWindow
 
         if (GUI.Button(new Rect(new Vector2(5, 70),new Vector2(70,45)), "Click me"))
         {
-
             if (myPrefab != null)
             {
-                
-                Instantiate(myPrefab, objPos, Quaternion.identity);
+                lastPlacedObject = (GameObject)Instantiate(myPrefab, objPos, Quaternion.identity);
+                //Instantiate(myPrefab, objPos, Quaternion.identity);
+                //Debug.Log(lastPlacedObject);
+                //var nameLastPlacedObj = PrefabUtility.GetCorrespondingObjectFromSource(myPrefab);
+                //--> Saves Object here.
+                string localPath = "Assets/Prefab/" + myPrefab.name +  ".prefab";
+          
+                //localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+                bool prefabSuccess;
+                if (AssetDatabase.IsValidFolder(localPath))
+                {
+                    PrefabUtility.SaveAsPrefabAsset(lastPlacedObject, localPath, out prefabSuccess);
+                    //Debug.Log(prefabSuccess ? "Successfully saved" : "Failed to save");
+                }
+                contentsRoot = PrefabUtility.LoadPrefabContents(localPath);
+
+
+                //SAVE EVERY DETAIL:
+                lastPlacedPos = lastPlacedObject.transform.position;
+                lastPlacedScale = lastPlacedObject.transform.lossyScale;
             }
             else
             {
@@ -62,6 +83,18 @@ public class PrefabWindow : EditorWindow
             objPos.Set(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
         }
 
+        if (GUI.Button(new Rect(new Vector2(0, 120), new Vector2(70, 45)), "Undo"))
+        {
+            DestroyImmediate(lastPlacedObject);
+            Debug.Log(contentsRoot);
+            Debug.Log("Undo");
+        }
+        if (GUI.Button(new Rect(new Vector2(0, 160), new Vector2(70, 45)), "Redo"))
+        {
+            Instantiate(contentsRoot, lastPlacedPos, Quaternion.identity);
+            //contentsRoot = null;
+            Debug.Log("Redo");
+        }
         EditorGUILayout.EndVertical();
 
     }
