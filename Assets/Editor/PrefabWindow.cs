00ab132a0e1b18636fa7ae6c7058cd0d5e28b272
Miazyn <94353989+Miazyn@users.noreply.github.com>
2022-07-27 15:09:47 +0200

    More options in window

--- Assets/Editor/PrefabWindow.cs
+++ Assets/Editor/PrefabWindow.cs
@@ -7,8 +7,10 @@ public class PrefabWindow : EditorWindow
 {
 
     GameObject myPrefab;
-    Vector3 objPos;
-    float radius = 1f;
+    static Vector3 objPos;
+    static Vector3 objScale;
+    
+    
     
     [MenuItem("Personal Tools/Prefab Editor")]
     public static void ShowWindow()
@@ -16,41 +18,53 @@ public class PrefabWindow : EditorWindow
         EditorWindow.GetWindow<PrefabWindow>("Prefab Editor");
     }
 
+    [InitializeOnLoadMethod]
+    static void Init()
+    {
+        SceneView.duringSceneGui += SceneView_duringSceneGui;
+    }
+
+    private static void SceneView_duringSceneGui(SceneView obj)
+    {
+        Handles.color = Color.red;
+        Handles.DrawWireCube(objPos, objScale);
+        obj.Repaint();
+    }
 
     private void OnGUI()
     {
+        
         EditorGUILayout.BeginVertical();
         myPrefab = EditorGUILayout.ObjectField("Prefab",myPrefab, typeof(GameObject), false) as GameObject;
         objPos = EditorGUILayout.Vector3Field("Object Position", objPos);
+
+        objScale = myPrefab != null ? myPrefab.transform.lossyScale : Vector3.zero;
         
+
         if (GUI.Button(new Rect(new Vector2(5, 70),new Vector2(70,45)), "Click me"))
         {
 
-            Debug.Log("Button clicked");
             if (myPrefab != null)
             {
-                //if(Physics.CheckBox(objPos, new Vector3(1,1,1)/2, Quaternion.identity))
-                //{
-                //    Debug.Log("Object alrdy placed here");
-                //}
-                //else
-                //{
-                //    Instantiate(myPrefab, objPos, Quaternion.identity);
-                //}
-
+                
                 Instantiate(myPrefab, objPos, Quaternion.identity);
             }
             else
             {
-                Debug.Log("Prefab empty!");
+                Debug.LogWarning("Prefab empty!");
             }
         }
 
         if(GUI.Button(new Rect(new Vector2(100, 70), new Vector2(150, 45)), "Randomize pos btw 1-10"))
         {
-            objPos.Set(Random.Range(0, 10), 0,0);
+            //Randomize values
+
+            objPos.Set(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
         }
 
         EditorGUILayout.EndVertical();
+
     }
+
+
 }
